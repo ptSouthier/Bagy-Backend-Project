7a3f20a3b710557d81b7da8b52a55e9615ee2afe
Southier <patrick.southier@hotmail.com>
2022-01-03 05:17:19 -0300

    feat: creating some validations to createCustomer mutation

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,10 @@
 const Customer = require('./database/models/Customer');
 
+const validateEmail = (email) => {
+  const emailPattern = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+  return emailPattern.test(email);
+};
+
 module.exports = {
   Query: {
     customers: () => Customer.findAll(),
@@ -7,9 +12,30 @@ module.exports = {
   },
 
   Mutation: {
-    createCustomer: (_, { data }) => {
+    createCustomer: async (_, { data }) => {
       const { name, email, cpf, birthday } = data;
       const { street, neighborhood, city, state, country, cep, number } = data;
+      const MIN_NAME_LENGTH = 3;
+      const VALID_CPF_LENGTH = 11;
+
+      if (name.length < MIN_NAME_LENGTH) {
+        throw new Error("Name way too short! Try your full name.");
+      }
+      
+      if (!validateEmail(email)) {
+        throw new Error("Email must be a valid email.");
+      }
+
+      if (cpf.length !== VALID_CPF_LENGTH) {
+        throw new Error("Invalid CPF entry.");
+      }
+      
+      const emailAlreadyInUse = await Customer.findOne({ where: { email } });
+      
+      if (emailAlreadyInUse) {
+        throw new Error("Email is already in use.");
+      }
+
 
       const customerObj = {
         name,
